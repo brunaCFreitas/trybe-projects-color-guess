5030f63ae95d044c4be6731df07aded348af9c99
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-05 20:12:38 -0300

    Adiciona requisito bônus

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const paragrafo = document.getElementById('rgb-color');
 const paletteColors = document.getElementById('palette-colors');
 const answer = document.getElementById('answer');
 const buttonRestart = document.getElementById('reset-game');
+const scoreElement = document.getElementById('score');
+
+let clickCounter = 0;
+
+function addPoint() {
+  const points = localStorage.getItem('points') || 0;
+  localStorage.setItem('points', +points + 3);
+}
+
+function updatePoints() {
+  const points = localStorage.getItem('points') || 0;
+  scoreElement.innerText = points;
+}
 
 function generateRandomColor() {
   const red = Math.floor(Math.random() * 255);
@@ -14,31 +27,55 @@ function generateRandomColor() {
 }
 
 function checkColors(event) {
-  if (event.target.style.backgroundColor === paragrafo.innerText) {
-    answer.innerText = 'Acertou!';
-  } else {
-    answer.innerText = 'Errou! Tente novamente!';
+  clickCounter += 1;
+
+  if (clickCounter > 1) {
+    return;
   }
+
+  const targetColor = event.target.style.backgroundColor;
+  const isCorrect = targetColor === paragrafo.innerText;
+  if (isCorrect) {
+    answer.innerText = 'Acertou!';
+    addPoint();
+    updatePoints();
+    return;
+  }
+
+  answer.innerText = 'Errou! Tente novamente!';
+}
+
+function addRandomColor(colors) {
+  const colorPalette = document.createElement('div');
+  colorPalette.className = 'ball';
+  const color = generateRandomColor();
+  colorPalette.style.backgroundColor = color;
+  paletteColors.addEventListener('click', checkColors);
+  colors.push(color);
+  paletteColors.appendChild(colorPalette);
+  paragrafo.innerText = color;
 }
 
 function createPaletteColors() {
-  const collors = [];
+  const colors = [];
   for (let index = 0; index < 6; index += 1) {
-    const colorPalette = document.createElement('div');
-    colorPalette.className = 'ball';
-    const color = generateRandomColor();
-    colorPalette.style.backgroundColor = color;
-    paletteColors.addEventListener('click', checkColors);
-    collors[index] = color;
-    paletteColors.appendChild(colorPalette);
-    paragrafo.innerText = color;
+    addRandomColor(colors);
   }
-  collors.sort();
-  paragrafo.innerText = collors[2];
+  colors.sort(() => 0.5 - Math.random());
+  paragrafo.innerText = colors.shift();
+}
+
+function restore() {
+  answer.innerText = 'Escolha uma cor';
+  paletteColors.innerHTML = '';
+  clickCounter = 0;
+  createPaletteColors();
+  updatePoints();
 }
 
 createPaletteColors();
+updatePoints();
 
 buttonRestart.addEventListener('click', () => {
-  location.reload();
+  restore();
 });
