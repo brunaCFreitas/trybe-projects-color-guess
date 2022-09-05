255237a0b5a44f443a9de5fcfb865c0685c38bd2
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-05 19:31:18 -0300

    Adiciona requisito 6

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const paragrafo = document.getElementById('rgb-color');
 const paletteColors = document.getElementById('palette-colors');
+const answer = document.getElementById('answer');
+const buttonRestart = document.getElementById('reset-game');
 
 function generateRandomColor() {
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
 
-  const color = `(${red}, ${green}, ${blue})`;
+  const color = `rgb(${red}, ${green}, ${blue})`;
 
   return color;
 }
 
-function createPaletteColors() {
-  for (let index = 0; index < 6; index += 1) {
-    const colorPalette = document.createElement('div');
-    colorPalette.className = 'ball';
-    const color = `rgb${generateRandomColor()}`;
-    colorPalette.style.backgroundColor = color;
-    paletteColors.appendChild(colorPalette);
-    paragrafo.innerText = color;
+function checkColors(event) {
+  if (event.target.style.backgroundColor === paragrafo.innerText) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
   }
 }
 
+function createPaletteColors() {
+  const collors = [];
+  for (let index = 0; index < 6; index += 1) {
+    const colorPalette = document.createElement('div');
+    colorPalette.className = 'ball';
+    const color = generateRandomColor();
+    colorPalette.style.backgroundColor = color;
+    paletteColors.addEventListener('click', checkColors);
+    collors[index] = color;
+    paletteColors.appendChild(colorPalette);
+    paragrafo.innerText = color;
+  }
+  collors.sort();
+  paragrafo.innerText = collors[2];
+}
+
 createPaletteColors();
+
+buttonRestart.addEventListener('click', () => {
+  location.reload();
+});
